a5f0f015dcbe08db41ea27ca46fca455329c14fb
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-28 11:14:24 +0100

    ...

--- Java/DayTwo/Depreciation.java
+++ Java/DayTwo/Depreciation.java
@@ -1,7 +1,10 @@
 public class Depreciation {
 
 public Double getPrice(int numberOfItems) {
-	return 12.5;
+	Double total = numberOfItems * 50000.0;
+	Double deduction = total * 0.08;
+	Double times = 12.5;
+	return times;
 
 }
 
